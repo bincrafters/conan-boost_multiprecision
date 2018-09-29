3a80191bc19d6da8d467cba3743eacb9303211c8
Jerry W <jerrywiltse@gmail.com>
2018-09-29 17:15:36 -0400

    move to python_requires strategy

--- conanfile.py
+++ conanfile.py
@@ -1,74 +1,33 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, tools
+from conans import python_requires
 
 
-class BoostMultiprecisionConan(ConanFile):
+base = python_requires("boost_base/1.67.0@bincrafters/testing")
+
+class BoostMultiprecisionConan(base.BoostBaseConan):
     name = "boost_multiprecision"
-    version = "1.67.0"
-    author = "Bincrafters <bincrafters@gmail.com>"
-    exports = ["LICENSE.md"]
-    lib_short_names = ["multiprecision"]
-    is_header_only = True
-
-    def package_id_additional(self):
-        self.info.header_only()
-
-
-    requires = (
-        "boost_array/1.67.0@bincrafters/testing",
-        "boost_assert/1.67.0@bincrafters/testing",
-        "boost_config/1.67.0@bincrafters/testing",
-        "boost_container_hash/1.67.0@bincrafters/testing",
-        "boost_core/1.67.0@bincrafters/testing",
-        "boost_integer/1.67.0@bincrafters/testing",
-        "boost_lexical_cast/1.67.0@bincrafters/testing",
-        "boost_math/1.67.0@bincrafters/testing",
-        "boost_mpl/1.67.0@bincrafters/testing",
-        "boost_package_tools/1.67.0@bincrafters/testing",
-        "boost_predef/1.67.0@bincrafters/testing",
-        "boost_random/1.67.0@bincrafters/testing",
-        "boost_rational/1.67.0@bincrafters/testing",
-        "boost_smart_ptr/1.67.0@bincrafters/testing",
-        "boost_static_assert/1.67.0@bincrafters/testing",
-        "boost_throw_exception/1.67.0@bincrafters/testing",
-        "boost_type_traits/1.67.0@bincrafters/testing"
-    )
-
-    # BEGIN
-
     url = "https://github.com/bincrafters/conan-boost_multiprecision"
-    description = "Please visit http://www.boost.org/doc/libs/1_67_0"
-    license = "BSL-1.0"
-    short_paths = True
-    build_requires = "boost_generator/1.67.0@bincrafters/testing"
+    lib_short_names = ["multiprecision"]
+    header_only_libs = ["multiprecision"]
+    b2_requires = [
+        "boost_array",
+        "boost_assert",
+        "boost_config",
+        "boost_container_hash",
+        "boost_core",
+        "boost_integer",
+        "boost_lexical_cast",
+        "boost_math",
+        "boost_mpl",
+        "boost_predef",
+        "boost_random",
+        "boost_rational",
+        "boost_smart_ptr",
+        "boost_static_assert",
+        "boost_throw_exception",
+        "boost_type_traits"
+    ]
 
-    def package_id(self):
-        getattr(self, "package_id_additional", lambda:None)()
 
-    def source(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.source(self)
-        getattr(self, "source_additional", lambda:None)()
-
-    def build(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.build(self)
-        getattr(self, "build_additional", lambda:None)()
-
-    def package(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package(self)
-        getattr(self, "package_additional", lambda:None)()
-
-    def package_info(self):
-        with tools.pythonpath(self):
-            import boost_package_tools  # pylint: disable=F0401
-            boost_package_tools.package_info(self)
-        getattr(self, "package_info_additional", lambda:None)()
-
-    # END
