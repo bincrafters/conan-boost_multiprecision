198c30e599abeafcab8774e0e8604330d64d698b
Rene Rivera <grafikrobot@gmail.com>
2018-11-29 22:01:12 -0600

    Initial 1.68.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,30 +4,12 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.67.0@bincrafters/testing")
+base = python_requires("boost_base/1.68.0@bincrafters/testing")
 
 class BoostMultiprecisionConan(base.BoostBaseConan):
     name = "boost_multiprecision"
     url = "https://github.com/bincrafters/conan-boost_multiprecision"
     lib_short_names = ["multiprecision"]
     header_only_libs = ["multiprecision"]
-    b2_requires = [
-        "boost_array",
-        "boost_assert",
-        "boost_config",
-        "boost_container_hash",
-        "boost_core",
-        "boost_integer",
-        "boost_lexical_cast",
-        "boost_math",
-        "boost_mpl",
-        "boost_predef",
-        "boost_random",
-        "boost_rational",
-        "boost_smart_ptr",
-        "boost_static_assert",
-        "boost_throw_exception",
-        "boost_type_traits"
-    ]
-
-
+    cycle_group = "boost_cycle_group_c"
+    b2_requires = ["boost_cycle_group_c"]
